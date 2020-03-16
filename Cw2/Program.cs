fa9cd2fcad138cf534ef06187a4424e499e18f17
s18969 <s18969@pjwstk.edu.pl>
2020-03-16 15:06:28 +0100

    Delete !

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -6,7 +6,7 @@ namespace Cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
