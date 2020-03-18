5629a271dd140fe0a6aff58d96b91a9c3d3fd96f
s18969 <s18969@pjwstk.edu.pl>
2020-03-18 10:44:09 +0100

    Add Student class

---
+++ Cw2/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw2
+{
+    class Student
+    {
+
+        public string FirsName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UniversityName { get; set; }
+
+        public string UniversityMode { get; set; }
+        
+        public string BirthDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string NameMother { get; set; }
+
+        public string NameFather { get; set; }
+
+
+    }
+}
