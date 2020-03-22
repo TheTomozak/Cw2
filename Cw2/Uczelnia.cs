33079beb66749bf3a533daaef170a26ae0808321
s18969 <s18969@pjwstk.edu.pl>
2020-03-22 20:38:59 +0100

    Added class Uczelnia

---
+++ Cw2/Uczelnia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Cw2
+{
+    [Serializable]
+    class Uczelnia
+    {
+        [XmlElement("CreatedAt")]
+        [JsonPropertyName("CreatedAt")]
+        public string CreatedAt { get; set; }
+
+        [XmlElement("Author")]
+        [JsonPropertyName("Author")]
+        public string Author { get; set; }
+
+        public List<Student> studentsList { get; set; }
+
+    }
+}
